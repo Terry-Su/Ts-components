db4c37231b72493cd6a3983b27954d5ef1c19461
TerrySu <2684759855@qq.com>
2017-10-24 21:54:49 +0800

    Update detail

--- src/ReactComponents/Dragger/component/App.js
+++ src/ReactComponents/Dragger/component/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 
 import RestrictContainer from './RestrictContainer'
-import Element from './Element'
+import Dragger from './Dragger'
 
 export default class App extends Component {
     render() {
         return (
             <div className="App">
-                {/* <Element /> */}
-                <RestrictContainer />
+                <Dragger>
+                    
+                </Dragger>
             </div>
         )
     }
